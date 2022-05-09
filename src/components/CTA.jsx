e3126a7e78e5b7e42d920f03c83ac3923022725e
coffmanjrp <coffmanjrp@gmail.com>
2022-05-09 11:16:28 -0700

    Fix CTA button

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from 'react';
+
 const CTA = ({ children, href, variant }) => {
-  const red = ['text-white', 'bg-brightRed', 'hover:bg-brightRedLight'];
-  const white = [
-    'bg-white',
-    'shadow-2xl',
-    'text-brightRed',
-    'hover:bg-gray-300',
-  ];
-  const variantClasses = variant === 'red' ? red : white;
+  const [variantClasses, setVariantClasses] = useState([]);
+
+  useEffect(() => {
+    switch (variant) {
+      case 'red':
+        setVariantClasses([
+          'text-white',
+          'bg-brightRed',
+          'hover:bg-brightRedLight',
+        ]);
+        break;
+      case 'white':
+        setVariantClasses([
+          'bg-white',
+          'shadow-2xl',
+          'text-brightRed',
+          'hover:bg-gray-300',
+        ]);
+        break;
+      case 'gray':
+        setVariantClasses([
+          'bg-gray-600',
+          'text-white',
+          'hover:bg-white',
+          'hover:text-brightRed',
+        ]);
+        break;
+      default:
+        setVariantClasses([]);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <a
