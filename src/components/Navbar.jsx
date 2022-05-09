f2b2cc58eaba482f0436d85bce197a733272494f
coffmanjrp <coffmanjrp@gmail.com>
2022-05-09 12:05:10 -0700

    Hamburger Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { CTA } from '.';
 import { logo } from '../img';
 
@@ -10,6 +11,8 @@ const navLinks = [
 ];
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <nav className="relative container mx-auto p-6">
       <div className="flex items-center justify-between">
@@ -32,6 +35,31 @@ const Navbar = () => {
           <CTA href="#!" variant="red">
             Get Started
           </CTA>
+        </div>
+        <button
+          type="button"
+          className={`${
+            openMenu && 'open'
+          } block hamburger md:hidden focus:outline-none`}
+          onClick={() => setOpenMenu((prevState) => !prevState)}
+        >
+          <span className="hamburger-top"></span>
+          <span className="hamburger-middle"></span>
+          <span className="hamburger-bottom"></span>
+        </button>
+      </div>
+      <div className="md:hidden">
+        <div
+          className={`${
+            openMenu ? 'flex' : 'hidden'
+          } absolute flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}
+        >
+          {navLinks.length > 0 &&
+            navLinks.map((link, index) => (
+              <a key={index} href={link.href}>
+                {link.title}
+              </a>
+            ))}
         </div>
       </div>
     </nav>
